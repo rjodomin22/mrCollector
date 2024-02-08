f82ef74b2a64b1f8e9805a08cbe7fe0263bbc5b5
Sergio Jurado <juradotrillosergio1207@gmail.com>
2024-02-08 18:25:33 +0100

    Login/Register DONE

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -29,7 +29,7 @@ class RegistrationController extends AbstractController
                     $form->get('plainPassword')->getData()
                 )
             );
-
+            $user->setState(0);
             $entityManager->persist($user);
             $entityManager->flush();
             // do anything else you need here, like send an email
