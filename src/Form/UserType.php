f95e851dd543e8c4669c7add970315303f6b29db
Jose Miguel Garcia <josegarmal1811@gmail.com>
2024-02-08 18:31:35 +0100

    crud de User

---
+++ src/Form/UserType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Stack;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('username')
+            ->add('roles')
+            ->add('password')
+            ->add('email')
+            ->add('state')
+            ->add('subscribedStacks', EntityType::class, [
+                'class' => Stack::class,
+'choice_label' => 'id',
+'multiple' => true,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
