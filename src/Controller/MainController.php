f2dfd2290a0d5bedf01d97029c03417ae3bcd757
josemanuelcordon <josemanuelcordoncastillo01@gmail.com>
2024-02-12 00:08:48 +0100

    Before stash

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -12,7 +12,6 @@ class MainController extends AbstractController
     public function index(): Response
     {
         return $this->render('main/index.html.twig', [
-            'controller_name' => 'MainController',
         ]);
     }
 }
